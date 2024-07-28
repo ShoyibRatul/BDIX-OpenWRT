571e3a0802954e2c57c4df871270544154f29638
Md Shoyib Ratul <86622610+ShoyibRatul@users.noreply.github.com>
2024-07-29 01:18:39 +0600

    update

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ opkg update
 opkg install iptables iptables-mod-nat-extra redsocks
 
 #Then run this line
-service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/emonbhuiyan/Socks5-OpenWRT/raw/main/Socks5.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/emonbhuiyan/Socks5-OpenWRT/raw/main/Socks5 && chmod +x /etc/init.d/Socks5
+service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://raw.githubusercontent.com/ShoyibRatul/BDIX-OpenWRT/main/Socks5.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://raw.githubusercontent.com/ShoyibRatul/BDIX-OpenWRT/main/Socks5 && chmod +x /etc/init.d/Socks5
 
 cd /
 clear
 
 
-echo -e "Thanks for installing. Follow me for more updates: https://fb.me/emoncontact"
+echo -e "Thanks for installing. CH3CKM8"
